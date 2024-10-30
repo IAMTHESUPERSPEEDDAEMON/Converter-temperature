f328ab22c659d254210f9933805df381ba934327
OSwego <syava23223@gmail.com>
2024-10-30 22:28:25 +0200

    Add Farenheit to Celsius method

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,5 +3,17 @@ package app;
 public class Main  {
     public static void main (String[] args) {
         System.out.println("Temperature converter app");
+
+        double a = 68;
+        double b = 33;
+
+        double celsius = FarenheitToC(a);
+        System.out.println("68F to C = " + celsius + "C");
+    }
+
+    public static double FarenheitToC (double a) {
+        double result = (a - 32) * 5/9;
+
+        return result;
     }
 }
